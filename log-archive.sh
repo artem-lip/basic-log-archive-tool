0b5675905bb7e14be699df07763e1bfae06c882d
Artem Lip <ametas-cloud@proton.me>
2024-08-30 11:06:47 +0300

    feat: add date and time feature in log-archive.sh file

--- log-archive.sh
+++ log-archive.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 directory=$1
-tar -czf "${directory}.tar.gz" $directory
+date=$(date +%Y%m%d)
+current_hour=$(date +%H)
+current_minute=$(date +%M)
+current_second=$(date +%S)
+seconds_since_midnight=$(( current_hour * 3600 + current_minute * 60 + current_second ))
+tar -czf "${directory}_$date_$seconds_since_midnight.tar.gz" $directory
