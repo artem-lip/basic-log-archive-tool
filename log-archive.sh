bebf1833efe29d0a3f7901243a86395796a245c0
Artem Lip <ametas-cloud@proton.me>
2024-08-30 11:52:05 +0300

    fix: fix not showing seconds in archives names in log-archive.sh file

--- log-archive.sh
+++ log-archive.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 directory=$1
 date=$(date +%Y%m%d)
-current_hour=$(date +%H)
-current_minute=$(date +%M)
-current_second=$(date +%S)
-seconds_since_midnight=$(( current_hour * 3600 + current_minute * 60 + current_second ))
-tar -czf "${directory}_$date_$seconds_since_midnight.tar.gz" $directory
+current_time=$(date +%H%M%S)
+tar -czf "${directory}_$(echo "$date")_$(echo "$current_time").tar.gz" $directory
